4d37a4842416761d9930396085a87e798dc2bdcd
charlesmariga <charlesmariga37@gmail.com>
2022-04-23 00:53:22 +0300

    Refactored some code

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int printIdentifiers(char next, va_list arg)
 		{"s", print_str},
 		{"i", print_int},
 		{"d", print_int},
-		{"b", print_unsignedToBinary},
+		{"b", print_binary},
 		{NULL, NULL}
 	};
 
@@ -83,34 +83,5 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(arg);
-	return (charPrinted);
-}
-
-/**
- * print_int - prints an integer.
- * @arg: argument
- * Return: 0
- */
-
-int print_int(va_list arg)
-{
-	unsigned int divisor = 1, i, resp, charPrinted = 0;
-	int n = va_arg(arg, int);
-
-	if (n < 0)
-	{
-		_putchar('-');
-		charPrinted++;
-		n *= -1;
-	}
-
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-	;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
 	return (charPrinted);
 }
--- main.h
+++ main.h
@@ -8,8 +8,7 @@ int _putchar(char c);
 int print_char(va_list character);
 int print_str(va_list string);
 int print_int(va_list arg);
-int print_unsignedToBinary(va_list arg);
-void print_binary(unsigned int n, unsigned int *printed);
+int print_binary(va_list list);
 
 /**
  * struct formatstruct - structure definition of a printTypeStruct
@@ -21,5 +20,11 @@ typedef struct formatstruct
 	char *indentifier;
 	int (*printer)(va_list);
 } formatstruct;
+
+/* Helper functions */
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *s);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
